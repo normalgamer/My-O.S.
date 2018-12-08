d7766bb2479d4651c2254c8bc8050efe014a3ba7
normalgamer <36603719+normalgamer@users.noreply.github.com>
2018-12-08 21:10:51 +0100

    Add files via upload

--- loader.s
+++ loader.s
@@ -15,3 +15,13 @@
         mov eax, 0xCAFEBABE         ; place the number 0xCAFEBABE in the register eax
     .loop:
         jmp .loop                   ; loop forever
+
+KERNEL_STACK_SIZE equ 4096                  ; size of stack in bytes
+
+section .bss
+align 4                                     ; align at 4 bytes
+kernel_stack:                               ; label points to beginning of memory
+    resb KERNEL_STACK_SIZE                  ; reserve stack for the kernel
+    
+mov esp, kernel_stack + KERNEL_STACK_SIZE   ; point esp to the start of the
+                                            ; stack (end of memory area)
